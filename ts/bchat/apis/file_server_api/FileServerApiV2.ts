16aa0716afdc16093abdca4d443494dc5160b5e9
munavver <munavver@beldex.io>
2022-10-11 16:34:25 +0530

    chat screen ui issuse fixed and version changed and chart screen image download issues fixed

--- ts/bchat/apis/file_server_api/FileServerApiV2.ts
+++ ts/bchat/apis/file_server_api/FileServerApiV2.ts
@@ -7,15 +7,15 @@ import { parseStatusCodeFromOnionRequest } from '../open_group_api/opengroupV2/O
 
 //  testnet file server
 
-// export const fileServerV2URL = 'http://13.233.252.86';
+ //export const fileServerV2URL = 'http://13.233.252.86';
 
-  // export const fileServerV2PubKey =
-    //   '51a8cd2e8a2c7e012d2ed4e0a24b5ed26f6f4f5b89e0d9d9c8856a5fd7140314';
+ //export const fileServerV2PubKey =
+   //'51a8cd2e8a2c7e012d2ed4e0a24b5ed26f6f4f5b89e0d9d9c8856a5fd7140314';
 
 
   // mainet file server
 
- export const fileServerV2URL = 'http://fs1.rpcnode.stream';
+export const fileServerV2URL = 'http://fs1.rpcnode.stream';
 
  export const fileServerV2PubKey =
    'f3024b309be838eff764c6804c417b667096d6c5301184f90fb66e9e4515444c';
@@ -57,6 +57,7 @@ export const uploadFileToFsV2 = async (
   };
 
   const result = await sendApiV2Request(request);
+  console.log('result imgage',result)
   const statusCode = parseStatusCodeFromOnionRequest(result);
   if (statusCode !== 200) {
     return null;
@@ -68,6 +69,8 @@ export const uploadFileToFsV2 = async (
     return null;
   }
   const fileUrl = `${fileServerV2URL}/${FILES_ENDPOINT}/${fileId}`;
+  console.log('filurl image',fileUrl);
+  
   return {
     fileId: fileId,
     fileUrl,
