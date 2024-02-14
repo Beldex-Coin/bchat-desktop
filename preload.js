d150e97d0b49fae704be240cd2c5fe3d1186f1b3
alexsanchez@beldex.io <alexsanchez@beldex.io>
2024-02-14 18:21:11 +0530

    New contact bns validation completed.

--- preload.js
+++ preload.js
@@ -235,7 +235,7 @@ window.React = require('react');
 window.ReactDOM = require('react-dom');
 window.clipboard = clipboard;
 
-window.networkType = 'mainnet';
+window.networkType = 'testnet';
 
 if (window.networkType == 'mainnet') {
   window.getSeedNodeList = () =>
