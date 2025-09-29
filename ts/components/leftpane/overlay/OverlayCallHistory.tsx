0fc3432da783f29de740de3cb2c0d67ddc047d38
munavver <munavver@beldex.io>
2025-09-29 18:43:28 +0530

    feature to implement to enable direct calls from call icon in call history.

--- ts/components/leftpane/overlay/OverlayCallHistory.tsx
+++ ts/components/leftpane/overlay/OverlayCallHistory.tsx
@@ -3,10 +3,10 @@ import { SpacerLG, SpacerMD, SpacerXS } from '../../basic/Text';
 import { BchatIcon, BchatIconButton } from '../../icon';
 import classNames from 'classnames';
 import {
-  useConversationBnsHolder,
+  // useConversationBnsHolder,
   useConversationUsernameOrShorten,
 } from '../../../hooks/useParamSelector';
-import { AvatarItem } from '../../wallet/BchatWalletAddressBook';
+// import { AvatarItem } from '../../wallet/BchatWalletAddressBook';
 import { Flex } from '../../basic/Flex';
 import { useSelector } from 'react-redux';
 
@@ -18,12 +18,35 @@ import { getCallHistoryList } from '../../../state/selectors/callHistory';
 import ContactEmptyIcon from '../../icon/ContactEmptyIcon';
 import { getTheme } from '../../../state/selectors/theme';
 import styled from 'styled-components';
+import { getHasIncomingCall, getHasOngoingCall } from '../../../state/selectors/call';
+import { callRecipient } from '../../../interactions/conversationInteractions';
+import { Avatar, AvatarSize } from '../../avatar/Avatar';
 
+const style: any = {
+  'missed-call': {
+    notificationText: 'Missed Call',
+    iconType: 'callMissed',
+    iconColor: '#FF3E3E',
+
+  },
+  'started-call': {
+    notificationText: 'Outgoing',
+    iconType: 'callOutgoing',
+    iconColor: 'var(--color-text)',
+  },
+  'answered-a-call': {
+    notificationText: 'Incoming',
+    iconType: 'callIncoming',
+    iconColor: 'var(--color-text)',
+  },
+};
 export const OverlayCallHistory = () => {
   const callHistoryList = useSelector(getCallHistoryList);
   const [currentSearchTerm, setCurrentSearchTerm] = useState('');
+  const hasIncomingCall = useSelector(getHasIncomingCall);
+  const hasOngoingCall = useSelector(getHasOngoingCall);
+  const canCall = !(hasIncomingCall || hasOngoingCall);
 
-  // derived value instead of extra state
   const filteredList = useMemo(() => {
     if (!currentSearchTerm) return callHistoryList;
 
@@ -39,35 +62,38 @@ export const OverlayCallHistory = () => {
   };
   const openConverstation = async (pubKey: string, messageId: string) => {
     await openConversationWithMessages({ conversationKey: pubKey, messageId: messageId });
+    void callRecipient(pubKey, canCall);
   };
   return (
-    <div className="module-left-pane-overlay" style={{overflow:'hidden'}}>
+    <div className="module-left-pane-overlay" style={{ overflow: 'hidden' }}>
       <div className="module-left-pane-overlay-call-history">
         <SpacerLG />
-        <div className="module-left-pane-overlay-call-history--header">Call History</div>
-        <SpacerMD />
-        <div className="bchat-search-input">
-          <div className="search">
-            <BchatIcon iconSize={20} iconType="search" />
+        <div className="module-left-pane-overlay-call-history--header">
+          <div className="module-left-pane-overlay-call-history--header-txt">Call History</div>
+          <SpacerMD />
+          <div className="bchat-search-input">
+            <div className="search">
+              <BchatIcon iconSize={20} iconType="search" />
+            </div>
+            <input
+              value={currentSearchTerm}
+              onChange={e => {
+                handleSearch(e);
+              }}
+              placeholder={'Search People'}
+              maxLength={26}
+            />
           </div>
-          <input
-            value={currentSearchTerm}
-            onChange={e => {
-              handleSearch(e);
-            }}
-            placeholder={'Search People'}
-            maxLength={26}
-          />
         </div>
         <SpacerMD />
-        <div className='content-box-wrapper '>
-        {filteredList.length === 0 ? (
-          <SearchEmptyScreen />
-        ) : (
-          filteredList.map((item, key) => (
-            <ContactList key={key} openConverstation={openConverstation} {...item} />
-          ))
-        )}
+        <div className="content-box-wrapper ">
+          {filteredList.length === 0 ? (
+            <SearchEmptyScreen />
+          ) : (
+            filteredList.map((item, key) => (
+              <ContactList key={key} openConverstation={openConverstation} {...item} />
+            ))
+          )}
         </div>
       </div>
     </div>
@@ -83,25 +109,25 @@ const ContactList = (props: any) => {
     openConverstation,
   } = props;
   const username = useConversationUsernameOrShorten(conversationId);
-  const isBnsHolder = useConversationBnsHolder(conversationId);
+  // const isBnsHolder = useConversationBnsHolder(conversationId);
 
-  const callCategoryText =
-    callNotificationType === 'missed-call'
-      ? 'Missed Call'
-      : callNotificationType === 'started-call'
-      ? 'Outgoing'
-      : 'Incoming';
+  const styleItem = style[callNotificationType];
+  const callCategoryText = styleItem.notificationText;
+  const iconType = styleItem.iconType;
+  const iconColor = styleItem.iconColor;
+
   const subtext = `${callCategoryText}${
     call_details.length > 1 ? ` (${call_details.length})` : ''
-  } - ${formatMessageTime(received_at)}`;
+  }`;
   const lastCallMessageId = call_details[0].messageId;
+  
 
   return (
     <>
-      <div className={classNames(`content-box }`)} style={{ cursor: 'pointer' }}>
-        <Flex container={true}>
+      <div className={classNames(`content-box`)} >
+        <Flex container={true} alignItems='center'>
           <div className="avatarBox">
-            <AvatarItem memberPubkey={conversationId} isBnsHolder={isBnsHolder} />
+          <Avatar size={AvatarSize.L} pubkey={conversationId} />
           </div>
 
           <Flex container={true} flexDirection="column" margin="0 15px">
@@ -110,11 +136,17 @@ const ContactList = (props: any) => {
             </div>
             <SpacerXS />
 
-            <div className={'address'}>{subtext}</div>
+            <div className={'address'} >
+              <BchatIcon iconType={iconType} iconSize={14} iconColor={iconColor} />
+              <span style={{ margin: '0 5px',color: iconColor }}>
+              {subtext}
+              </span>
+              - {formatMessageTime(received_at)}
+            </div>
           </Flex>
         </Flex>
         <BchatIconButton
-          iconSize={31}
+          iconSize={24}
           iconType={'call'}
           onClick={() => openConverstation(conversationId, lastCallMessageId)}
           fillRule="evenodd"
@@ -142,7 +174,6 @@ const SearchEmptyWrapper = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
-
 `;
 const StyledSpan = styled.span`
   color: #a7a7ba;
@@ -160,8 +191,8 @@ const formatMessageTime = (timestamp: number) => {
   if (m.isSame(now, 'day')) {
     return m.format('hh:mm A'); // Today → time
   } else if (m.isSame(now, 'year')) {
-    return m.format('DD MMMM'); // Same year → day + month
+    return m.format('DD MMM'); // Same year → day + month
   } else {
-    return m.format('DD MMMM YY'); // Different year → day + month + year
+    return m.format('DD MMM YY'); // Different year → day + month + year
   }
 };
