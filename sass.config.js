22deeb9821be0114a7e6087f25ac10702d3ee012
munavver-dev <munavver.dev@gmail.com>
2025-12-11 15:44:24 +0530

    feature to  replace Grunt & Parcel with Webpack configuration for build.

---
+++ sass.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+const sass = require('sass'); // Prefer `dart-sass`
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'stylesheets', 'dist'),
+  },
+  entry: './stylesheets/manifest.scss',
+  mode: 'production',
+
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `main.css` compiling all of the compiled css files
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          {
+            loader: 'css-loader',
+            options: {
+              url: false, // Disable url() processing since we don't have asset imports
+            },
+          },
+          // Compiles Sass to CSS
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: sass,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [].concat(
+    new MiniCssExtractPlugin({
+      filename: 'manifest.css',
+    })
+  ),
+};
